fcb3ccae8f71cb023ce67b2b58a81c02403bfbb2
will <willafolabi78@gmail.com>
2022-09-02 00:53:37 +0200

    mad functions to determine the size of  the sketchpad

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /* eslint-disable linebreak-style */
-const amountOfGrids = 16 * 16;
-const sketchBox = document.getElementById('sketch-pad');
+const sketchBox = document.querySelector('#sketch-pad');
+const defaultSize = midSize();
 
 function creatediv() {
   const createGrid = document.createElement('div');
@@ -8,15 +8,100 @@ function creatediv() {
   sketchBox.appendChild(createGrid);
 }
 
-for (let i = 0; i < amountOfGrids; i += 1) {
-  creatediv();
-}
-
 function changeBackground(e) {
   e.currentTarget.style.backgroundColor = 'white';
 }
 
-const grids = document.querySelectorAll (".grid");
-grids.forEach(function(grid){
+const select = document.querySelector('#select-pad-size');
+
+function changePadSize(e){
+  if (select.value === 'small size') {
+    smallSize();
+    //console.log(horizontalGrids);
+  } else if (select.value === 'medium size') {
+    midSize()
+  } else if (select.value === 'large size') {
+    largeSize()
+  } else{
+    console.log('idk')
+  }
+
+ // kick()
+}
+
+/* function kick(){
+
+
+  //let finalPadSize = horizontalGrids * verticleGrids;
+
+  for (let i = 1; i <= finalPadSize; i += 1) {
+    creatediv();
+  }
+
+  const grids = document.querySelectorAll (".grid");
+  grids.forEach(function(grid){
   grid.addEventListener("mouseover", changeBackground);
-});
+  });
+
+}
+ */
+select.addEventListener('change', changePadSize);
+
+
+
+
+/*  might use
+const sketchPadSize = document.querySelector('.sketch-pad-size');
+const smallPad = document.querySelector('.small-size-pad');
+const mediumPad = document.querySelector('.medium-size-pad');
+const largePad = document.querySelector('.large-size-pad');
+
+const fitPageBtn = document.querySelector('.full-page-pad'); // add event listener to set pad to full page
+ */
+
+function smallSize() {
+  const horizontalGrids = 8;
+  const verticleGrids = horizontalGrids;
+  const finalPadSize = horizontalGrids * verticleGrids;
+  sketchBox.style.setProperty('grid-template-columns', `repeat(${horizontalGrids}, auto)`);
+  sketchBox.style.setProperty('grid-template-rows', `repeat(${horizontalGrids}, auto)`);
+
+  for (let i = 1; i <= 8 * 8; i += 1) {
+    creatediv();
+  }
+
+  const grids = document.querySelectorAll('.grid');
+  grids.forEach((grid) => { grid.addEventListener('mouseover', changeBackground); });
+}
+
+function midSize() {
+  const horizontalGrids = 16;
+  const verticleGrids = horizontalGrids;
+  const finalPadSize = horizontalGrids * verticleGrids;
+  sketchBox.style.setProperty('grid-template-columns', `repeat(${horizontalGrids}, auto)`);
+  sketchBox.style.setProperty('grid-template-rows', `repeat(${horizontalGrids}, auto)`);
+
+  for (let i = 1; i <= finalPadSize; i += 1) {
+    creatediv();
+  }
+
+  const grids = document.querySelectorAll('.grid');
+  grids.forEach((grid) => { grid.addEventListener('mouseover', changeBackground); });
+}
+
+
+
+function largeSize() {
+  const horizontalGrids = 32;
+  const verticleGrids = horizontalGrids;
+  const finalPadSize = horizontalGrids * verticleGrids;
+  sketchBox.style.setProperty('grid-template-columns', `repeat(${horizontalGrids}, auto)`);
+  sketchBox.style.setProperty('grid-template-rows', `repeat(${horizontalGrids}, auto)`);
+
+  for (let i = 1; i <= finalPadSize; i += 1) {
+    creatediv();
+  }
+
+  const grids = document.querySelectorAll('.grid');
+  grids.forEach((grid) => { grid.addEventListener('mouseover', changeBackground); });
+}
